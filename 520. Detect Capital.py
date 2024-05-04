0b7769f2a6dca7ce1c72f9cab09ecd9fceb52aca
mohit <mohitmolela@gmail.com>
2024-05-04 16:01:41 +0530

    Detect Capital

---
+++ 520. Detect Capital.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        if word.isupper():
+            return True
+
+        if word.islower():
+            return True
+        
+        elif word[0].isupper() and word[1:].islower():
+            return True
+        
+        return False
