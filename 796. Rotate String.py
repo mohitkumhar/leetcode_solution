bda95efc7a4f24683a2c69f4edaa7b83b01f766a
Mohit Kumhar <mohitmolela@gmail.com>
2024-05-22 17:08:43 +0530

    796. Rotate String.py

---
+++ 796. Rotate String.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+
+        double_s = (s + s)
+        return goal in double_s
