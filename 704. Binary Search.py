f2d92c820f23b2219930a98dc13676925a60d857
Mohit Kumhar <mohitmolela@gmail.com>
2024-05-23 15:36:56 +0530

    Binary Search

---
+++ 704. Binary Search.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if target > nums[mid] and target <= nums[right]:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return -1
