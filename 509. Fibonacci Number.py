e147f82e9c23ba8cf07cdf6fcf3d8c84547859dc
Mohit Kumhar <mohitmolela@gmail.com>
2024-07-27 21:32:24 +0530

    .\509. Fibonacci Number.py

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        if n == 0:
+            return 0
+
+        elif n == 1:
+            return 1
+
+        elif n == 2:
+            return 1
+
+        return self.fib(n - 1) + self.fib(n - 2)
