7fa25e6b149d2ab91f7149820255a08f977e2d02
Mohit Kumhar <mohitmolela@gmail.com>
2024-08-01 10:42:38 +0530

    .\2678. Number of Senior Citizens.py

--- 2678. Number of Senior Citizens.py
+++ 2678. Number of Senior Citizens.py
@@ -1,18 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        sum = 0
 
-driver = webdriver.Chrome()
+        for detail in details:
+            if detail[-4:-2] > '60':
+                sum += 1
 
-query = 'laptop'
-
-driver.get(f'https://www.amazon.in/s?k={query}&crid=27P9HZWM3O3AI&sprefix=lapt%2Caps%2C271&ref=nb_sb_noss_2')
-
-elem = driver.find_element(By.CLASS_NAME, 'puisg-row')
-
-# print(elem.text)  # this will extract all text from given class
-
-print(elem.get_attribute('outerHTML')) # this will give full html inside that class
-
-driver.close()
+        return sum
