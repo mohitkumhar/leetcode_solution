48936802457c77860bdaf70cba0a3c6fc7649984
Mohit Kumhar <mohitmolela@gmail.com>
2024-09-18 13:20:21 +0530

    .\179. Largest Number.py

---
+++ 179. Largest Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        nums = list(map(str, nums))
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] < nums[j] + nums[i]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                
+        result = ''.join(nums)
+
+        if result[0] == '0':
+            return '0'
+
+        return result
