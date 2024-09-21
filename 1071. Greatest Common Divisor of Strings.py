62a210ae6ebdf405076cfe87f63de17b8f9058db
Mohit Kumhar <mohitmolela@gmail.com>
2024-09-21 18:53:34 +0530

    .\1071. Greatest Common Divisor of Strings.py

---
+++ 1071. Greatest Common Divisor of Strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        gcd_sum = math.gcd(len(str1), len(str2))
+
+        return str1[:gcd_sum]
+
+
