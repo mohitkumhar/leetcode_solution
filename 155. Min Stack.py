cb714a3c1a0e8042506cd147bd33a472da06b80b
Mohit Kumhar <mohitmolela@gmail.com>
2024-11-10 23:04:33 +0530

    .\155. Min Stack.py

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -2,14 +2,25 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_stack = []
         
 
     def push(self, val: int) -> None:
         self.stack.append(val)
         
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        
 
     def pop(self) -> None:
-        self.stack.pop()
+        
+        if self.stack:
+            if self.stack[-1] == self.min_stack[-1]:
+                self.min_stack.pop()
+
+            self.stack.pop()
+
+
         
 
     def top(self) -> int:
@@ -20,7 +31,8 @@ class MinStack:
         
 
     def getMin(self) -> int:
-        return min(self.stack)
+        if self.min_stack:
+            return self.min_stack[-1]
         
 
 
