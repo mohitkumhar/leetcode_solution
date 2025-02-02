4b14f84728147587364235554f59eba65ddde0a8
Mohit Kumhar <mohitmolela@gmail.com>
2025-02-02 23:44:48 +0530

    .\1752. Check if Array Is Sorted and Rotated.py

---
+++ 1752. Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,14 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        count = 0
+        for i in range(n):
+        
+            if nums[i] > nums[(i + 1) % n]:
+                count += 1
+                
+                if count > 1:
+                    return False
+        
+        return True
