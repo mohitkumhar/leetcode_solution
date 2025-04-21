be5c7adc651d5642fd98b02d3d713a6ff746d1bb
Mohit Kumhar <mohitmolela@gmail.com>
2025-04-21 16:45:06 +0530

    .\240. Search a 2D Matrix II.py

---
+++ 240. Search a 2D Matrix II.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        for row in matrix:
+            for val in row:
+                if val == target:
+                    return True
+        
+        return False
+        
