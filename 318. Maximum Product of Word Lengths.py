a355b47b7b35cd6e7512ad486378b6fe6881988c
Mohit Kumhar <mohitmolela@gmail.com>
2025-05-05 10:58:34 +0530

    .\318. Maximum Product of Word Lengths.py

---
+++ 318. Maximum Product of Word Lengths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        def words_match(word1, word2):
+            word2 = set(word2)
+            for w in word1:
+                if w in word2:
+                    return False
+            return True
+
+        max_len = 0
+
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if words_match(words[i], words[j]):
+                    max_len = max(max_len, len(words[i]) * len(words[j]))
+
+        return max_len
