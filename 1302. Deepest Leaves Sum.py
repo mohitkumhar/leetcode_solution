4d86ca9546c16b2e30e13cd30471b51f17729146
Mohit Kumhar <mohitmolela@gmail.com>
2025-06-01 21:09:01 +0530

    .\1302. Deepest Leaves Sum.py

---
+++ 1302. Deepest Leaves Sum.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        result = []
+        queue = [root]
+
+        while queue:
+            next_level = []
+            for node in queue:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            if not next_level:
+                return sum(n.val for n in queue)
+
+            queue = next_level
