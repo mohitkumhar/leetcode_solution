e709549dc997cdfbafba816e9b052cab1ec5e9c9
Mohit Kumhar <mohitmolela@gmail.com>
2025-06-06 18:55:11 +0530

    .\637. Average of Levels in Binary Tree.py

---
+++ 637. Average of Levels in Binary Tree.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        result = []
+        queue = [root]
+
+        while queue:
+            next_level = []
+            temp = []
+
+            for node in queue:
+                temp.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            result.append(sum(temp) / len(temp))
+            queue = next_level
+
+        return result
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        result = []
+        queue = [root]
+
+        while queue:
+            next_level = []
+            temp = []
+
+            for node in queue:
+                temp.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            result.append(sum(temp) / len(temp))
+            queue = next_level
+
+        return result
