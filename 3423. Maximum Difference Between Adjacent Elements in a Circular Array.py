4159229f1a91c2f21b6860d414ccca1860cd1108
Mohit Kumhar <mohitmolela@gmail.com>
2025-06-12 22:57:02 +0530

    .\3423. Maximum Difference Between Adjacent Elements in a Circular Array.py

--- 3423. Maximum Difference Between Adjacent Elements in a Circular Array.py
+++ 3423. Maximum Difference Between Adjacent Elements in a Circular Array.py
@@ -3,6 +3,6 @@ class Solution:
         result = 0
         for i in range(1, len(nums)):
             result = max(result, abs(nums[i - 1] - nums[i]))
-        
+
         result = max(result, abs(nums[0] - nums[len(nums) - 1]))
         return result
