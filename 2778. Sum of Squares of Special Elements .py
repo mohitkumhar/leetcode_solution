5ab6c6ff04f0fe524f087d70abda21334738e8b2
Mohit Kumhar <mohitmolela@gmail.com>
2025-06-28 21:39:49 +0530

    .\2778. Sum of Squares of Special Elements .py

---
+++ 2778. Sum of Squares of Special Elements .py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+        for i in range(1, n + 1):
+            if n % i == 0:
+                ans += nums[i - 1] * nums[i - 1]
+        return ans
