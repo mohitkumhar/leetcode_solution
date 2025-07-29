2424fa1bcdf0bf4ae6222b47d76a5c4dc88bf022
Mohit Kumhar <mohitmolela@gmail.com>
2025-07-30 00:17:35 +0530

    .\2744. Find Maximum Number of String Pairs.py

---
+++ 2744. Find Maximum Number of String Pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        count = 0
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if words[i] == words[j][::-1]:
+                    count += 1
+        return count
