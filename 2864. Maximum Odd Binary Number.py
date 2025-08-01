d7e91b0c141a1c937a247627a30396bdbfeb701f
Mohit Kumhar <mohitmolela@gmail.com>
2025-08-01 20:37:49 +0530

    .\2864. Maximum Odd Binary Number.py

---
+++ 2864. Maximum Odd Binary Number.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones = s.count('1')
+        zeros = s.count('0')
+        return (ones - 1) * '1' + (zeros) * '0' + '1'
