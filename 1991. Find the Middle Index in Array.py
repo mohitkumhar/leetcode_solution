2713c2a3a3f7c6cf45bd8f980e583df2dd28fa94
Mohit Kumhar <mohitmolela@gmail.com>
2025-11-26 16:28:23 +0530

    1991. Find the Middle Index in Array

---
+++ 1991. Find the Middle Index in Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        right_sum = sum(nums)
+
+        for i, num in enumerate(nums):
+            right_sum -= num
+
+            if left_sum == right_sum:
+                return i
+            left_sum += num
+        return -1
