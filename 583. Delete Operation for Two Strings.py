227e7a559bc5a2dd94861310c0bb351b4d617bb5
Mohit Kumhar <mohitmolela@gmail.com>
2026-05-03 14:45:52 +0530

    lc: #310 Implement minDistance method for string deletion
    closes #310

---
+++ 583. Delete Operation for Two Strings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        m = len(word1)
+        n = len(word2)
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        lcs = dp[m][n]
+
+        return (m + n) - (lcs * 2)
