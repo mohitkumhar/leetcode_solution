7d719f510ed9de06c2bb1e190121a9d7ecca0d39
Mohit Kumhar <mohitmolela@gmail.com>
2026-05-06 16:14:49 +0530

    gfg: #332 Add longest common substring solution (#333)
    closes #332

---
+++ gfg/Longest Common Substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longCommSubstr(self, s1, s2):
+        
+        m = len(s1)
+        n = len(s2)
+        
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] == 1 + dp[i - 1][j - 1]
+                    ans = max(ans, dp[i][j])
+                else:
+                    dp[i][j] = 0
+        
+        print(dp)
