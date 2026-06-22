ec252c036f609d4609cfb731be171867e422b08a
Mohit Kumhar <mohitmolela@gmail.com>
2026-06-23 03:39:22 +0530

    Array 2246-longest-path-with-different-adjacent-characters Hard 477 ms (30.11%) 109.3 MB (7.17%)

--- Python3/Hard/2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ Python3/Hard/2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,19 +1,21 @@
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
+
         n = len(parent)
         matrix = {i: [] for i in range(n)}
 
         for i in range(1, n):
             matrix[parent[i]].append(i)
 
-        def dfs(node):
+        def dfs(node, parent):
             max1 = 0
             max2 = 0
-
             for child in matrix.get(node, []):
-                length = dfs(child)
+                if child == parent:
+                    continue
+                length = dfs(child, node)
 
-                if s[child] == s[node]:
+                if s[node] == s[child]:
                     continue
 
                 if length > max1:
@@ -26,8 +28,10 @@ class Solution:
 
             return max1 + 1
 
+
         self.ans = 0
-        dfs(0)
+        dfs(0, -1)
 
         return self.ans
-        
+
+
