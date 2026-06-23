7d1b2d97bbe558e1fbb0707cc27af06263b3786c
Mohit Kumhar <mohitmolela@gmail.com>
2026-06-23 15:10:57 +0530

    Depth-First Search 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero Medium 197 ms (17.55%) 50.3 MB (58.23%)

---
+++ Python3/Medium/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero-06-23-2026-15-10-55.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        matrix = {i: [] for i in range(n)}
+        direction = {i: [] for i in range(n)}
+
+        for u, v in connections:
+            matrix[u].append(v)
+            matrix[v].append(u)
+
+            direction[u].append(v)
+
+        queue = deque([0])
+        count = 0
+        visited = set()
+
+        while queue:
+            node = queue.popleft()
+            if node in visited:
+                continue
+            visited.add(node)
+
+            for nei in matrix.get(node, []):
+                if nei not in visited:
+                    if nei in direction[node]:
+                        count += 1
+
+                    queue.append(nei)
+
+        return count
