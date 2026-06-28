920794a79c46c2087eea5f939ce776de8a4f2436
Mohit Kumhar <mohitmolela@gmail.com>
2026-06-28 06:51:59 +0530

    Array 3737-count-subarrays-with-majority-element-i Medium 1778 ms (45.3%) 19.6 MB (22.84%)

---
+++ Python3/Medium/3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i-06-28-2026-06-51-58.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        count = 0
+        result = 0
+
+        for i in range(n):
+            count = 0
+            for j in range(i, n):
+                if nums[j] == target:
+                    count += 1
+                length = j - i + 1
+                if (2 * count) > length:
+                    result += 1
+        return result
+
+
