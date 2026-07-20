d2e2e2ab212ea536dbec2fb39a3a23afbe38fdde
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-20 23:12:11 +0530

    Array | Medium | 0189-rotate-arrayPython3 | 3 ms (73.56%) | 26.5 MB (43.97%) 07-20-2026

---
+++ Python3/Medium/0189-rotate-array/0189-rotate-array-07-20-2026-23-12-08.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+
+        def reverse(nums, i, j):
+
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
