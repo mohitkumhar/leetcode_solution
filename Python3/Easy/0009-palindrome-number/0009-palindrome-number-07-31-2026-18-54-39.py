e1d26a23d7003e2a838d52f20e212e589c723784
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 18:54:40 +0530

    Math | Easy | 0009-palindrome-numberPython3 | 3 ms (88.85%) | 19.3 MB (53.91%) 07-31-2026

---
+++ Python3/Easy/0009-palindrome-number/0009-palindrome-number-07-31-2026-18-54-39.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        i = 0
+        j = len(x) - 1
+
+        while i < j:
+            if x[i] != x[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
