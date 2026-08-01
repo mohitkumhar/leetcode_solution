e55e09bf4b4a57e80f950133536017f351d38ae5
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-01 16:39:17 +0530

    Array | Medium | 0486-predict-the-winnerPython3 | 0 ms (100%) | 19.6 MB (38.19%) 08-01-2026

---
+++ Python3/Medium/0486-predict-the-winner/0486-predict-the-winner-08-01-2026-16-39-14.py
@@ -0,0 +1,27 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        def solve(i, j):
+            if i > j:
+                return 0
+
+            if i == j:
+                return nums[i]
+            if memo[i][j] != -1:
+                return memo[i][j]
+
+            take_i = nums[i] + min(solve(i + 2, j), solve(i + 1, j - 1))
+
+            take_j = nums[j] + min(solve(i, j - 2), solve(i + 1, j - 1))
+
+            memo[i][j] = max(take_i, take_j)
+            return memo[i][j]
+
+        n = len(nums)
+
+        memo = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
+
+        player1_score = solve(0, len(nums) - 1)
+        player2_score = sum(nums) - player1_score
+
+        return player1_score >= player2_score
