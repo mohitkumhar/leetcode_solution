e6f1899841b9621af69b544c789fd380baea41b0
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-01 19:13:56 +0530

    Array | Medium | 3532-path-existence-queries-in-a-graph-iC++ | 7 ms (69.53%) | 224.1 MB (69.99%) 08-01-2026

---
+++ C++/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i-08-01-2026-19-13-51.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
+        vector<int> component(n, -1);
+        int compId = 0;
+        component[0] = compId;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                compId++;
+            }
+            component[i] = compId;
+        }
+
+        vector<bool> result;
+        for (auto& query : queries) {
+            result.push_back(component[query[0]] == component[query[1]]);
+        }
+        return result;
+    }
+};
