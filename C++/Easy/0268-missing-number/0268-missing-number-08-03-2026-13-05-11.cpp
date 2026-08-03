b18f5d1abf41b40cf3f8011ac4682ed0db239fc5
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-03 13:05:13 +0530

    Array | Easy | 0268-missing-numberC++ | 0 ms (100%) | 21.7 MB (94.36%) 08-03-2026

---
+++ C++/Easy/0268-missing-number/0268-missing-number-08-03-2026-13-05-11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        if (nums[0] != 0)
+            return 0;
+
+        for (int i = 1; i < n; i++) {
+            if ((nums[i] - nums[i - 1]) > 1)
+                return i;
+        }
+        return n;
+    }
+};
