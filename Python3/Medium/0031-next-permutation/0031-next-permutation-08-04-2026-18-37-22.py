3b9565adfb6b1c99da7596bcd3c4eec255cd2147
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-04 18:37:24 +0530

    Array | Medium | 0031-next-permutationPython3 | 0 ms (100%) | 19.2 MB (91.72%) 08-04-2026

---
+++ Python3/Medium/0031-next-permutation/0031-next-permutation-08-04-2026-18-37-22.py
@@ -0,0 +1,29 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        breakPoint = -1
+
+        for i in range(n - 1, 0, -1):
+            if nums[i - 1] < nums[i]:
+                breakPoint = i - 1
+                break
+
+        if breakPoint == -1:
+            nums.reverse()
+            return
+
+        for i in range(n - 1, breakPoint - 1, -1):
+            if nums[i] > nums[breakPoint]:
+                nums[i], nums[breakPoint] = nums[breakPoint], nums[i]
+                break
+
+        left = breakPoint + 1
+        right = n - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
