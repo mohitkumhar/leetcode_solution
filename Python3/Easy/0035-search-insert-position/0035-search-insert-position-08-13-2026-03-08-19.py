29f9c5b0384818c20e70f721376af24c5e9a2a8b
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-13 03:08:21 +0530

    Array | Easy | 0035-search-insert-positionPython3 | 0 ms (100%) | 19.9 MB (43.13%) 08-13-2026

---
+++ Python3/Easy/0035-search-insert-position/0035-search-insert-position-08-13-2026-03-08-19.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        n = len(nums)
+
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
