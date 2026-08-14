4411974dd249a94762fec5cdb0c6bc710e0735e4
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-15 00:42:46 +0530

    Array | Medium | 0162-find-peak-elementPython3 | 0 ms (100%) | 19.3 MB (79.26%) 08-15-2026

---
+++ Python3/Medium/0162-find-peak-element/0162-find-peak-element-08-15-2026-00-42-44.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        if n <= 2:
+            return nums.index(max(nums))
+
+        left = 0
+        right = n - 1
+
+        while left < right:
+
+            mid = left + (right - left) // 2
+
+            if mid > 0 and mid < (n - 1) and nums[mid - 1] < nums[mid] > nums[mid + 1]:
+                return mid
+
+            elif nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
