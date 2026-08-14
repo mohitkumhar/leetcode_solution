17636fb63f868caea84ad16a314bcfa77486157d
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-14 22:13:24 +0530

    Array | Medium | 0540-single-element-in-a-sorted-arrayPython3 | 0 ms (100%) | 26.9 MB (61.7%) 08-14-2026

---
+++ Python3/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array-08-14-2026-22-13-21.py
@@ -0,0 +1,25 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        left = 0
+        right = n - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == nums[mid + 1]:
+                if (right - mid) % 2 == 1:  # if elements are odd from the curr idx
+                    right = mid - 1
+                else:
+                    left = mid + 2
+
+            else:
+                if (right - mid) % 2 == 1:  # if elements are odd from the curr idx
+                    left = mid + 1
+                else:
+                    right = mid
+
+        return nums[right]
