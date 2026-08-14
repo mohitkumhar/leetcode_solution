950f32cef6e43da3092930c145bc864ab8bd67be
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-14 17:55:38 +0530

    Array | Medium | 0081-search-in-rotated-sorted-array-iiC++ | 0 ms (100%) | 18.1 MB (8.65%) 08-14-2026

---
+++ C++/Medium/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii-08-14-2026-17-55-35.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+
+            // left half is sorted
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+
+            // right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
