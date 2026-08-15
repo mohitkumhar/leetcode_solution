7f74c9821d72607e6846a7bd299a4a70f56799d7
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-15 23:58:56 +0530

    Array | Medium | 1011-capacity-to-ship-packages-within-d-daysPython3 | 222 ms (48.38%) | 23.7 MB (99.12%) 08-15-2026

---
+++ Python3/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days-08-15-2026-23-58-54.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+
+        def isPossible(maxWeight):
+
+            currDays = 1
+            currWeight = 0
+
+            for weight in weights:
+
+                if currWeight + weight > maxWeight:
+                    currDays += 1
+                    currWeight = weight
+
+                else:
+                    currWeight += weight
+
+            return currDays <= days
+
+        left = max(weights)
+        right = sum(weights)
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isPossible(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
