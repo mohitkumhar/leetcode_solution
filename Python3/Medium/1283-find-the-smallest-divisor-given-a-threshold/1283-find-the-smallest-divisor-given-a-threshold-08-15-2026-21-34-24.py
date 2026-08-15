a0b15891a7eb1de0ac034fc58d95a0a59ed3ef48
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-15 21:34:27 +0530

    Array | Medium | 1283-find-the-smallest-divisor-given-a-thresholdPython3 | 147 ms (25%) | 24.4 MB (37.47%) 08-15-2026

---
+++ Python3/Medium/1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold-08-15-2026-21-34-24.py
@@ -0,0 +1,28 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+
+        def isPossible(val):
+            count = 0
+
+            for num in nums:
+                flag = 0
+
+                if num % val != 0:
+                    flag = 1
+
+                count += (num // val) + flag
+
+            return count <= threshold
+
+        left = 1
+        right = max(nums)
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isPossible(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
