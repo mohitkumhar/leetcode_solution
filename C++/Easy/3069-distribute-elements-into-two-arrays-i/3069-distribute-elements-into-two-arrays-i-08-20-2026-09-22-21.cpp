93465d423465ee3007836e79ac548200c4356ab9
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-20 09:22:30 +0530

    Array | Easy | 3069-distribute-elements-into-two-arrays-iC++ | 2 ms (27.58%) | 23.6 MB (97.31%) 08-20-2026

---
+++ C++/Easy/3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i-08-20-2026-09-22-21.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+
+        vector<int> arr1;
+        vector<int> arr2;
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back())
+                arr1.push_back(nums[i]);
+            else
+                arr2.push_back(nums[i]);
+        }
+
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        return arr1;
+    }
+};
