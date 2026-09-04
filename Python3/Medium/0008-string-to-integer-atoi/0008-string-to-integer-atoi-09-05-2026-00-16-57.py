61f5db3ae7339b702e59f1bbceec8087cc392d06
Mohit Kumhar <mohitmolela@gmail.com>
2026-09-05 00:17:01 +0530

    String | Medium | 0008-string-to-integer-atoi.Python3 | 0 ms (100%) | 19.3 MB (81.32%)

---
+++ Python3/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi-09-05-2026-00-16-57.py
@@ -0,0 +1,27 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        n = len(s)
+        sign = 1
+        i = 0
+        result = 0
+
+        while i < n and s[i] == " ":
+            i += 1
+
+        if i < n and (s[i] == "-" or s[i] == "+"):
+            if s[i] == "-":
+                sign = -1
+            i += 1
+
+        while i < n and s[i].isdigit():
+            result = result * 10 + int(s[i])
+            i += 1
+
+        result *= sign
+
+        if result < -2**31:
+            return -2**31
+        if result > 2**31 - 1:
+            return 2**31 - 1
+        
+        return result
