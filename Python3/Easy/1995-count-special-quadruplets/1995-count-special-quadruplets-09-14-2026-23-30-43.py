0c360a9331ee198c59285017761b212c5ec3900e
Mohit Kumhar <mohitmolela@gmail.com>
2026-09-14 23:30:50 +0530

    Array | Easy | 1995-count-special-quadruplets.Python3 | 503 ms (53.16%) | 19.2 MB (60.26%)

---
+++ Python3/Easy/1995-count-special-quadruplets/1995-count-special-quadruplets-09-14-2026-23-30-43.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    for l in range(k + 1, n):
+                        if nums[i] + nums[j] + nums[k] == nums[l]:
+                            count += 1
+        return count
