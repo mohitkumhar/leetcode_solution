2ce4a9deac9efa3bc0e1739dca8d1b54a3537356
Mohit Kumhar <mohitmolela@gmail.com>
2026-09-22 00:30:35 +0530

    Database | Easy | 0577-employee-bonus.MySQL | 1039 ms (71.22%) | 0B (100%)

---
+++ MySQL/Easy/0577-employee-bonus/0577-employee-bonus-09-22-2026-00-30-32.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT 
+    e.name,
+    b.bonus
+
+FROM Employee AS e
+LEFT JOIN Bonus AS b
+ON e.empId = b.empId
+WHERE b.bonus < 1000
+OR b.bonus IS NULL;
